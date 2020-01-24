15d01ff8fbedd1349add44291b7d025f6d3d1e59
Kelly Bazella <kelly.bazella@gmail.com>
2020-01-23 18:32:24 -0600

    saved to local storage, need to figure out color aspect

--- script.js
+++ script.js
@@ -1,53 +1,83 @@
 // variables
 var displayDate = document.getElementById("date-time");
 var display = moment().format("LLL");
-var times = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm"];
-var btnNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-var toDo = $("#enter-task")
-var userInput = [];
+var times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
+var btnNumber = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+var toDo = document.querySelectorAll(".enter-task");
+var userInputArr = Array(10);
 
-// display date
+
+
+var rightNow = moment().get("hour");
+console.log(rightNow);
+
+
 displayDate.append(display);
 
-//create div input areas using for loop 
-for (var i = 0; i<times.length;i++){
-    var newRow = $("<div>");
-    var saveButtons = $("<button>");
-    var inputArea = $("<input>")
-    var time = $("<div>");
-    inputArea.addClass("col-md-8 enter-task");
-    newRow.addClass("row");
-    time.addClass("col-md-2 times");
-    saveButtons.addClass("col-md-2");
-    saveButtons.attr("id", "save-button");
-    saveButtons.attr("value", btnNumber[i]);
-    inputArea.attr("index", btnNumber[i]);
-    time.attr("data-area", times[i]);
-    time.text(times[i]);
-    saveButtons.text("Save")
-    newRow.append(time);
-    newRow.append(inputArea);
-    newRow.append(saveButtons);
-    $(".container").append(newRow);
-}
-
-// functions
-function storeUserInput(){
-    localStorage.setItem("user-input", userInput)
-}
+$().ready(function () {
 
 
-//store users input to localStorage
-$("#save-button").on("click", function(event){
-    event.preventDefault();
-    var toDoText = toDo.innerHTML;
-    if(toDoText===""){
-        alert("You haven't entered anything to save.")
-    }else{
-        alert("got it!")
-        // save input to local storage
-        // put text in object??
-        // getItem on refresh
+    function renderColor() {
+        for (var t = 0; t < times.length; t++) {
+
+            if (rightNow < (btnNumber[t])) {
+                //grey
+                $("#input-"+times[t]).css("background-color", "#BABABA")
+            }
+            else if (rightNow === btnNumber[t]) {
+                //red
+                $("#input-"+times[t]).css("background-color", "#F95858")
+            } else {
+                //green
+                $("input").css("background-color", "#80ED67")
+            }
+        }
     }
+ 
+    if (localStorage.getItem("user-input") !== null) {
+        userInputArr = JSON.parse(localStorage.getItem("user-input"))
+    }
+    for (var i = 0; i < times.length; i++) {
+        var newRow = $("<div>");
+        var saveButtons = $("<button>");
+        var inputArea = $("<input>")
+        var time = $("<div>");
+        inputArea.addClass("col-md-8 enter-task before at-time-of after");
+        newRow.addClass("row");
+        time.addClass("col-md-2 times");
+        saveButtons.addClass("col-md-2 save-button");
+        saveButtons.attr("value", btnNumber[i]);
+        inputArea.attr("index", btnNumber[i]);
+        inputArea.attr("value", userInputArr[i]);
+        inputArea.attr("id", "input-"+times[i]);
+        time.attr("data-area", times[i]);
+        time.text(times[i]);
+        saveButtons.text("Save")
+        newRow.append(time);
+        newRow.append(inputArea);
+        newRow.append(saveButtons);
+        $(".container").append(newRow);
+       
+    }
+    renderColor();
 })
 
+
+
+function storeUserInput() {
+    localStorage.setItem("user-input", JSON.stringify(userInputArr))
+}
+
+$(document).on("click", ".save-button", function (event) {
+    event.preventDefault();
+    var toDoText = $(`[index="${event.target.value}"]`).val();
+    console.log(toDoText)
+    if (toDoText === "") {
+        return;
+    } else {
+        alert("Got it!")
+        userInputArr[event.target.value] = toDoText
+        storeUserInput();
+        
+    }
+})
